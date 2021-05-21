0e8b1102282fdd04516a6d946706478a7255288d
Omer Sabah <omerrj99@gmail.com>
2021-05-21 16:30:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const countDown=(n)=>{
 
 countDown(10);
 
+<<<<<<< HEAD
 const writeCards=(names,event)=>{
+=======
+const writeCards=(names.event)=>{
+>>>>>>> 16cf9fd3ff9a1db0320cad5a2891c33406a73481
   
   for(let i=0;i<names.length;i++){
     names[i]=`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
